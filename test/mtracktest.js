e1c6fe6fde95135334f3de24a09ea237e01e0075
leocyx <leo8400725@yahoo.com.tw>
2018-01-11 15:40:51 +0800

    travis test

---
+++ test/mtracktest.js
@@ -0,0 +1,24 @@
+//var Application = require('spectron').Application
+var assert = require('assert')
+var M = require("../work")
+
+describe('application launch', function () {
+    it('show total eat add100',function(){
+      assert.equal('100',M.eat('100'))
+     })
+    it('show total traffic add50',function(){
+         assert.equal('150',M.traffic('50'))
+    })
+    it('show total add10',function(){
+     assert.equal('160',M.leisure('10'))
+    })
+    it('show date and description and money',function(){
+        assert.equal('1/11 | 搭捷運 | 15元',M.event_description('搭捷運','15'))
+       })
+       it('show date and description and money',function(){
+        assert.equal('1/11 | 午餐 | 65元',M.event_description('午餐','65'))
+       })
+       it('show date and description and money',function(){
+        assert.equal('1/11 | 買衣服 | 790元',M.event_description('買衣服','790'))
+       })
+})
