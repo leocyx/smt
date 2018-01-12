1a6dbdbb20bd2a0bf650303bba01aa21a259cc41
leocyx <leo8400725@yahoo.com.tw>
2018-01-12 13:12:41 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 const {app, BrowserWindow} = require("electron")
 const path = require("path");
-app.on("ready", createwindow);
+const url = require('url')
+
+let win
 
 function createwindow(){
-    var win = new BrowserWindow({width:800, height:600})
-    win.loadURL(path.join(__dirname, "/index.html"))
+    win = new BrowserWindow({width: 800, height: 600})
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+    })
 }
-//app.on("ready", createwindow);
+
+app.on("ready", createwindow);
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
